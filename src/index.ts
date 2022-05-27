20b3fa80e9e4309fd37c2c5f300971f0f29ac1ef
danjerezTdf <104039810+danjerezTdf@users.noreply.github.com>
2022-05-27 15:15:38 -0300

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,10 @@
+/* Ejercicio – Par/Impar
+• Desarrollar un algoritmo que dado un número,
+ingresado por el usuario determine si el
+número es par o impar y le informe al usuario
+• En el caso de ser 0 (cero) el algoritmo deberá
+informarlo */
+
 let numeroValor: number = Number(prompt(`Ingreser Valor a comprobar`));
 console.log(numeroValor);
 if (numeroValor === 0) {
